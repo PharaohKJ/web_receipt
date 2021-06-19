054e88a2218d3b49fdf88f16bce11cb50ba1728d
PharaohKJ <kato@phalanxware.com>
2021-06-19 17:13:50 +0900

    本番に向けてファイルをあわせた

--- app/jobs/batch_file_to_receipt_job.rb
+++ app/jobs/batch_file_to_receipt_job.rb
@@ -6,14 +6,14 @@ class BatchFileToReceiptJob < ApplicationJob
     BatchFile.last.file.attachment.open do |b|
       CSV.foreach(b) do |c|
         r = Receipt.new(
+          no: c[0],
           customer_name: c[1],
-          no: Time.now.to_i,
-          price: Time.now.to_i,
-          issuer_name: c[2],
-          issuer_address: c[3],
-          proviso: c[1],
-          billing_date: Time.zone.parse(c[0]),
-          uuid: c[4].present? ? c[4] : SecureRandom.uuid
+          price: c[2].to_i,
+          issuer_name: c[3],
+          issuer_address: c[4],
+          proviso: c[5],
+          billing_date: Time.zone.parse(c[6]),
+          uuid: c[7].present? ? c[7] : SecureRandom.uuid
         )
         r.save
       end
