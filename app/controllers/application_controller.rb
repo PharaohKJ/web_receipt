286d45380b1df9f304552b234fac63ef4586d88b
PharaohKJ <kato@phalanxware.com>
2021-06-19 17:19:16 +0900

    basic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      user = ENV["BASIC_AUTH_USER"] || 'NTKANAZAWA-USER'
+      pass = ENV["BASIC_AUTH_PASSWORD"] || '16b4030a-bff7-465b-bacd-b42a9a75e403'
+      username == user && password == pass
+    end
+    end
 end
