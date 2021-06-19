2bcc150605163175b88d6575018e055c6d3a5fac
PharaohKJ <kato@phalanxware.com>
2021-06-19 17:40:12 +0900

    Production時のみ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth, if: :production?
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       user = ENV["BASIC_AUTH_USER"] || 'NTKANAZAWA-USER'
