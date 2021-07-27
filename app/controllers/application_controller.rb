e455040b179e5537c13e5c7a5a71d38ccf91aa3a
PharaohKJ <kato+github@phalanxware.com>
2021-07-27 22:21:22 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      user = ENV["BASIC_AUTH_USER"] || 'NTKANAZAWA-USER'
+      user = ENV["BASIC_AUTH_USER"] || 'WEB-RECEIPT-USER'
       pass = ENV["BASIC_AUTH_PASSWORD"] || '16b4030a-bff7-465b-bacd-b42a9a75e403'
       username == user && password == pass
     end
